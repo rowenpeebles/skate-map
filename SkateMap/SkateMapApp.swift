2c956e5c202730c67a39a90d3b5d56f98ec23be6
Rowen Peebles <rowen.peebles@outlook.com>
2022-05-30 16:10:22 +0100

    Initial Commit

---
+++ SkateMap/SkateMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkateMapApp.swift
+//  SkateMap
+//
+//  Created by Rowen Peebles on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SkateMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
